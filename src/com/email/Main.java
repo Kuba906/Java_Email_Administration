0cb0c778036a13e1172fe72eea37544937d35483
Kuba906 <63562993+Kuba906@users.noreply.github.com>
2021-07-15 13:57:29 +0200

    update java classes

---
+++ src/com/email/Main.java
@@ -0,0 +1,11 @@
+package com.email;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Email email = new Email("Jan","Kowalski");
+
+        System.out.println(email.showInfo());
+
+    }
+}
